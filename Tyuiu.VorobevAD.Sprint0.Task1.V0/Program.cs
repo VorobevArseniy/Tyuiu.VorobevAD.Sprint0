69e1d476185b0156d370718688b808b4d892e2a9
uiuiuiui15 <dr.venitus@gmail.com>
2024-09-12 00:39:59 +0500

    Task1

--- Tyuiu.VorobevAD.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.VorobevAD.Sprint0.Task1.V0/Program.cs
@@ -2,8 +2,12 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Hello, World!");
+
         string name = "Арсений";
         int age = 17;
-        Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+        Console.ReadKey();
     }
 }
