84396038c146edbca86394d74e5ced24de7d1681
uiuiuiui15 <dr.venitus@gmail.com>
2024-09-12 22:20:43 +0500

    Task3

---
+++ Tyu.VorobevAD.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VorobevAD.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.VorobevAD.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(5, 5));
+        }
+    }
+}
