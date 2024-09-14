e3746f7848fc3c00c1ecba42f6ead1e61dceb21c
uiuiuiui15 <dr.venitus@gmail.com>
2024-09-14 21:18:15 +0500

    Task6

---
+++ Tyuiu.VorobevAD.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿namespace Tyuiu.VorobevAD.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            int total = 0;
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                total += numbers[i];
+            }
+            return total;
+        }
+
+        public static object SubstractionArray(int[] numbers)
+        {
+            int total = 0;
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                total -= numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        public static object MultiplicatinArray(int[] numbers)
+        {
+            int total = 1;
+            int index = 0;
+            do
+            {
+                total *= numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+
+            return total;
+        }
+
+    }
+}
---
+++ Tyuiu.VorobevAD.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VorobevAD.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.VorobevAD.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("сумма бла бла бла..." + DataService.AdditionArray(arraynums));
+            Console.WriteLine("сумма бла бла бла..." + DataService.SubstractionArray(arraynums));
+            Console.WriteLine("сумма бла бла бла..." + DataService.MultiplicatinArray(arraynums));
+        }
+    }
+}
